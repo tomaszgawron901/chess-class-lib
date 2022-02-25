ac14304b753ee70ecc65cf0162362f546eb4cceb
tomaszgawron901 <mrgawron901@gmail.com>
2022-02-25 12:11:25 +0100

    add message to CouldNotPerformMoveException

--- ChessClassLib/Exceptions/GameExceptions.cs
+++ ChessClassLib/Exceptions/GameExceptions.cs
@@ -4,5 +4,8 @@ namespace ChessClassLibrary.Exceptions
 {
     public class CouldNotPerformMoveException: Exception
     {
+        public override string Message => "A move could not be performed.";
+
+        public override string ToString() => Message;
     }
 }
